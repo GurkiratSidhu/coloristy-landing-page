062822140cc55d0be64e8bedf59f53f92b9a632f
GurkiratSidhu <g3sidhu@gmail.com>
2021-07-23 19:06:39 +0530

    commit

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,29 @@
 import React from "react";
+import desk from "./Images/desk.jpg";
 
 function Header() {
 	return (
 		<>
-			<h1 className="text">Coloristy</h1>
+			<h1 className="flex justify-start mx-10 mt-12 font-medium text-gray-700">
+				Featured Projects
+			</h1>
+			<p className="flex justify-start mx-10 text-5xl mt-14 font-extralight">
+				Project Name
+			</p>
+			<p className="flex justify-start mx-10 text-5xl font-thin text-gray-400">
+				Looking under the hood
+			</p>
+			<p className="flex justify-start mx-10 font-bold text-gray-900 m-14">
+				More Projects
+			</p>
+
+			<div className="mx-10 ">
+				<img
+					className="duration-300 transform delay-50 hover:scale-90"
+					src={desk}
+					alt=""
+				></img>
+			</div>
 		</>
 	);
 }
--- src/Nav.js
+++ src/Nav.js
@@ -2,21 +2,38 @@ import React from "react";
 
 function Nav() {
 	return (
-		<div className="container bg-gray-200 ">
-			<div className="flex">
-				<h1 className="text-5xl font-bold text-gray-700 text-5xlxl ">
-					Coloristy
+		<div className="">
+			<nav className="flex justify-between p-8">
+				<h1 className="text-3xl font-bold text-gray-800 text-5xlxl">
+					Coloristy.
 				</h1>
-				<ul className="flex items-center p-5 text-xl font-bold text-gray-700 uppercase center ">
-					<li>Services</li>
-					<li>Clients</li>
-					<li>Careers</li>
-					<li>About</li>
+
+				<ul className="flex items-center text-xl font-bold text-gray-600 uppercase ">
+					<li>
+						<a href="/" className="mx-2 hover:text-gray-400">
+							Services
+						</a>
+					</li>
+					<li>
+						<a href="/" className="mx-2 hover:text-gray-400">
+							Clients
+						</a>
+					</li>
+					<li>
+						<a href="/" className="mx-2 hover:text-gray-400">
+							Careers
+						</a>
+					</li>
+					<li>
+						<a href="/" className="mx-2 hover:text-gray-400">
+							About
+						</a>
+					</li>
 				</ul>
-				<button className="px-6 py-2 text-4xl text-white bg-red-500 rounded-full ">
+				<button className="px-5 font-bold text-white uppercase bg-red-500 rounded-full text-m text-bold hover:bg-red-600">
 					Contact Us
 				</button>
-			</div>
+			</nav>
 		</div>
 	);
 }
