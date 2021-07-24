bab0e1736235eb4f33bbc8f278e97419f7200b52
GurkiratSidhu <g3sidhu@gmail.com>
2021-07-24 17:05:31 +0530

    new header layout

--- src/Header.js
+++ src/Header.js
@@ -4,26 +4,34 @@ import desk from "./Images/desk.jpg";
 function Header() {
 	return (
 		<>
-			<h1 className="flex justify-start mx-10 mt-12 font-medium text-gray-700">
-				Featured Projects
-			</h1>
-			<p className="flex justify-start mx-10 text-5xl mt-14 font-extralight">
-				Project Name
-			</p>
-			<p className="flex justify-start mx-10 text-5xl font-thin text-gray-400">
-				Looking under the hood
-			</p>
-			<p className="flex justify-start mx-10 font-bold text-gray-900 m-14">
-				More Projects
-			</p>
+			<section className="flex flex-col items-center md:flex-row justify-evenly">
+				<div className="p-2">
+					<p className="hidden text-6xl font-bold leading-snug text-left md:flex mt-14">
+						Design, <br></br> Development, <br></br> and identity for <br></br>
+						your product.
+					</p>
+					<p className="flex text-6xl font-bold leading-snug text-left md:hidden mt-14">
+						Design, Development, and identity for your product.
+					</p>
 
-			<div className="mx-10 ">
-				<img
-					className="duration-300 transform delay-50 hover:scale-90"
-					src={desk}
-					alt=""
-				></img>
-			</div>
+					<p className="flex mt-10 mb-5">
+						<a
+							href="/"
+							className="text-2xl font-bold text-blue-700 hover:text-blue-500 bg-auro "
+						>
+							Let's create something together.
+						</a>{" "}
+					</p>
+				</div>
+
+				<div className="p-2 md:w-1/2">
+					<img
+						className="duration-300 transform delay-50 hover:scale-90"
+						src={desk}
+						alt=""
+					></img>
+				</div>
+			</section>
 		</>
 	);
 }
--- src/Nav.js
+++ src/Nav.js
@@ -2,39 +2,57 @@ import React from "react";
 
 function Nav() {
 	return (
-		<div className="">
-			<nav className="flex justify-between p-8">
-				<h1 className="text-3xl font-bold text-gray-800 text-5xlxl">
-					Coloristy.
-				</h1>
-
-				<ul className="flex items-center text-xl font-bold text-gray-600 uppercase ">
+		<header>
+			<nav className="container flex items-center py-4 mt-4 sm:mt-8">
+				<div className="p-1">
+					<h1 className="pl-8 text-3xl font-bold text-gray-800 ">Coloristy.</h1>
+				</div>
+				<ul className="items-center justify-end flex-1 hidden gap-12 text-base text-gray-800 uppercase md:flex">
 					<li>
-						<a href="/" className="mx-2 hover:text-gray-400">
+						<a href="/" className="hover:text-gray-400">
 							Services
 						</a>
 					</li>
 					<li>
-						<a href="/" className="mx-2 hover:text-gray-400">
+						<a href="/" className="hover:text-gray-400">
 							Clients
 						</a>
 					</li>
 					<li>
-						<a href="/" className="mx-2 hover:text-gray-400">
+						<a href="/" className="hover:text-gray-400">
 							Careers
 						</a>
 					</li>
 					<li>
-						<a href="/" className="mx-2 hover:text-gray-400">
+						<a href="/" className="hover:text-gray-400">
 							About
 						</a>
 					</li>
+					<button
+						type="button"
+						className="px-4 py-2 font-medium text-white uppercase bg-red-500 rounded-full text-m text-bold hover:bg-red-600"
+					>
+						Contact Us
+					</button>
 				</ul>
-				<button className="px-5 font-bold text-white uppercase bg-red-500 rounded-full text-m text-bold hover:bg-red-600">
-					Contact Us
-				</button>
+				<div className="flex justify-end flex-1 md:hidden">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="w-6 h-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth={2}
+							d="M4 6h16M4 12h16M4 18h16"
+						/>
+					</svg>
+				</div>
 			</nav>
-		</div>
+		</header>
 	);
 }
 
